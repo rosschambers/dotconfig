4e1c26d681b997b8d8268d1c8a67d5a36afa6fc9
Ross Chambers <ross.a.chambers@gmail.com>
2024-06-30 16:59:00 -0400

    added github desktop

--- .nix/Core/Security/default.nix
+++ .nix/Core/Security/default.nix
@@ -2,4 +2,8 @@
 {
   environment.systemPackages =  with pkgs; [
   ];
+
+  security.sudo.extraConfig = ''
+    Defaults        timestamp_timeout=300
+  '';
 }
--- .nix/Packages/Development/default.nix
+++ .nix/Packages/Development/default.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages =  with pkgs; [
+    github-desktop
     nodejs
     node2nix
   ];
